760d494bf1a1f51ebc9cb53eb48079be91bce0a4
Aiken Tine Ahac <ahac.aiken@gmail.com>
2022-09-06 13:25:45 +0200

    Remove logs

--- lib/api/home/home.api.dart
+++ lib/api/home/home.api.dart
@@ -4,10 +4,8 @@ import 'package:app/api/profile/models/post.model.dart';
 class HomeAPI {
   Future<List<Post>> getPosts() async {
     final response = await ApiClient.getMany('/posts');
-    print(response);
 
     final List<Post> posts = List.from(response).map((e) {
-      print(e);
       return Post.fromJson(e);
     }).toList();
 
